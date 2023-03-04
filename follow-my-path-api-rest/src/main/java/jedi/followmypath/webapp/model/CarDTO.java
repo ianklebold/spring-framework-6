8b23f4b957e8337e87a2b849daf1877365c64202
ianklebold <ianstgo@gmail.com>
2023-03-04 17:07:45 -0300

    Tiny change

--- follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/model/CarDTO.java
+++ follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/model/CarDTO.java
@@ -1,9 +1,7 @@
 package jedi.followmypath.webapp.model;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
