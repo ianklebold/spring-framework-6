60ca51e35f1ad88f05296c9abc5342d2e7184ddf
ianklebold <ianstgo@gmail.com>
2023-03-04 18:29:24 -0300

    Section 9 finished

--- follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/entities/Car.java
+++ follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/entities/Car.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -22,7 +24,8 @@ public class Car {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36,columnDefinition = "varchar",updatable = false,nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR) //Aqui le decimos a Hibernate que el UUID lo mandemos como CHAR y no como Binary
+    @Column(length = 36,columnDefinition = "varchar(36)",updatable = false,nullable = false)
     private UUID id;
 
     @Version
--- follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/entities/Customer.java
+++ follow-my-path-api-rest/src/main/java/jedi/followmypath/webapp/entities/Customer.java
@@ -3,6 +3,8 @@ package jedi.followmypath.webapp.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,7 +19,8 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36,columnDefinition = "varchar",updatable = false,nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR) //Aqui le decimos a Hibernate que el UUID lo mandemos como CHAR y no como Binary
+    @Column(length = 36,columnDefinition = "varchar(36)",updatable = false,nullable = false)
     private UUID id;
 
     @Version
